2ba8c2972a2d9f50e52e8b427940680ca77d63a6
Thomas Ruehr <ruehr@in.tum.de>
2012-11-07 16:11:51 +0100

    kinematics plugin working

--- src/motion_planning.cpp
+++ src/motion_planning.cpp
@@ -4,6 +4,11 @@
 #include <arm_navigation_msgs/GetPlanningScene.h>
 #include <rosbag/bag.h>
 
+#include <kinematics_base/kinematics_base.h>
+
+#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_loader.h>
+
 ros::Publisher vis_marker_array_publisher_good_; //= rh.advertise<visualization_msgs::MarkerArray>("good_state_validity_markers_array", 128, true);
 
 static const std::string GET_PLANNING_SCENE_NAME = "/environment_server/get_planning_scene";
@@ -14,6 +19,47 @@ int main (int argc, char **argv)
     ros::init(argc, argv, "test_find_graspables");
 
     ros::NodeHandle nh_;
+
+    pluginlib::ClassLoader<kinematics::KinematicsBase> kinematics_loader_("kinematics_base","kinematics::KinematicsBase");//,"pr2_arm_kinematics::PR2ArmKinematicsPlugin");
+
+    kinematics::KinematicsBase * kinematics = NULL;
+
+    try
+    {
+        kinematics = kinematics_loader_.createClassInstance("pr2_arm_kinematics/PR2ArmKinematicsPlugin");
+        kinematics->initialize("right_arm",
+                               "torso_lift_link",
+                               "r_wrist_roll_link",
+                               0.1);
+        //            - Translation: [0.244, -0.542, 0.049]
+        //- Rotation: in Quaternion [-0.426, 0.091, 0.645, 0.628]
+        double tmp[] = {-1.6169497181369175, 0.15545771558980806, -1.250062782260915, -2.120402271101688, 11.660629107874348, -1.3540518577418752, -4.570108384006465};
+        std::vector<double> seed_state(tmp,tmp+7);
+        std::vector<double> solution(tmp,tmp+7);
+        int error_code = 0;
+
+        geometry_msgs::Pose pose;
+        tf::pointTFToMsg(tf::Point(0.244, -0.542, 0.049), pose.position);
+        tf::quaternionTFToMsg(tf::Quaternion(-0.426, 0.091, 0.645, 0.628), pose.orientation);
+
+        std::cout << "Pose " << pose << std::endl;
+
+        kinematics->getPositionIK(pose,
+                                  seed_state,
+                                  solution,
+                                  error_code);
+
+        std::cout << "Error code" << error_code << std::endl;
+        for (int k = 0; k < 7; k++)
+            std::cout << "joint " << k << " : " << solution[k] << std::endl;
+
+        //... use the polygon
+    }
+    catch(pluginlib::PluginlibException& ex)
+    {
+        //handle the class failing to load
+        ROS_ERROR("The plugin failed to load for some reason. Error: %s", ex.what());
+    }
 
     arm_navigation_msgs::GetPlanningScene::Response *planning_scene_res = 0;
     planning_scene_res = new arm_navigation_msgs::GetPlanningScene::Response ();
@@ -72,7 +118,8 @@ int main (int argc, char **argv)
         //collision_models->disableCollisionsForNonUpdatedLinks(long_arm_str[k] + "_arm");
         collision_models->disableCollisionsForNonUpdatedLinks("right_arm");
 
-        for (int k = 0; k < 2; k++) {
+        for (int k = 0; k < 2; k++)
+        {
             std::vector<std::string> temp = collision_models->getKinematicModel()->getModelGroup(long_arm_str[k] + "_arm")->getUpdatedLinkModelNames();
             arm_names[k].insert(arm_names[k].begin(), temp.begin(), temp.end());
         }
@@ -163,9 +210,9 @@ int main (int argc, char **argv)
             good_color.b = 1;
 
             //collision_models->getAllCollisionPointMarkers(*kinematic_state,
-              //      sum_arr,
-                //    good_color,
-                  //  ros::Duration(100));
+            //      sum_arr,
+            //    good_color,
+            //  ros::Duration(100));
 
             vis_marker_array_publisher_good_.publish(sum_arr);
         }
